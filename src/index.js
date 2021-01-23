a0156b83a16d44203afdd36c206f7bcb81fc6039
christopheveloupoule <christopheveloupoule@yahoo.fr>
2021-01-23 13:54:13 +0100

    Create route

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './style/index.scss';
-import { BrownserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 
 
 ReactDOM.render(
